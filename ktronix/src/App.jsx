47601fed7d645dfd50c0003c53a0b7ce5957b25e
marianaf2409 <marianacardonaf@gmail.com>
2025-12-11 19:28:55 -0500

    Servicios

--- ktronix/src/App.jsx
+++ ktronix/src/App.jsx
@@ -1,33 +1,44 @@
 import { Routes, Route } from "react-router-dom";
-import './App.css'
-import Login from './features/autenticacion/components/login'
-import Register from './features/autenticacion/components/register';
-import Dashboard from './features/modules/dashboard';
-import Clientes from './features/modules/Clientes.jsx';
-import Reservas from './features/modules/Reservas.jsx';
-import Servicios from './features/modules/Servicios.jsx';
-// Importa estos componentes cuando los crees:
-// import Servicios from './features/modules/Servicios.jsx';
-// import Reservas from './features/modules/Reservas.jsx';
+import "./App.css";
+
+// Autenticación
+import Login from "./features/autenticacion/components/login";
+import Register from "./features/autenticacion/components/register";
+
+// Módulos
+import Dashboard from "./features/modules/dashboard";
+import Clientes from "./features/modules/Clientes.jsx";
+import Reservas from "./features/modules/Reservas.jsx";
+import Servicios from "./features/modules/Servicios.jsx";
+
+// CRUD Servicios (importa estos archivos)
+import CrearServicio from "./features/modules/CrearServicio.jsx";
+import EditarServicio from "./features/modules/EditarServicio.jsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login/>} />
+        {/* LOGIN */}
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/clientes" element={<Clientes />} />
-        <Route path="/reservas" element={<Reservas />} />
-        <Route path="/servicios" element={<Servicios />} />
 
-        {/* Descomenta cuando tengas los componentes creados:
-        <Route path="/servicios" element={<Servicios />} />
+        {/* DASHBOARD */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* CLIENTES */}
+        <Route path="/clientes" element={<Clientes />} />
+
+        {/* RESERVAS */}
         <Route path="/reservas" element={<Reservas />} />
-        */}
+
+        {/* SERVICIOS CRUD */}
+        <Route path="/servicios" element={<Servicios />} />
+        <Route path="/servicios/crear" element={<CrearServicio />} />
+        <Route path="/servicios/editar/:id" element={<EditarServicio />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
